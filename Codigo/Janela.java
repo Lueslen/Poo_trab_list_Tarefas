e1b9b5bf14fde8628eb6ecd2998ea21def82b2bd
Lueslen Machado da Silva <lueslensilva@hotmail.com>
2022-11-02 21:14:33 -0300

    Update Janela.java

--- Codigo/Janela.java
+++ Codigo/Janela.java
@@ -3,9 +3,7 @@ import java.awt.*;
 
 public class Janela extends JFrame {
     private JLabel labelcaixa;
-
     private JButton tarefaButton;
-
     private JCheckBox Tarefa;
     private JCheckBox Tarefa1;
     private JCheckBox Tarefa2;
@@ -15,7 +13,7 @@ public class Janela extends JFrame {
         JFrame frame = new JFrame();
         setLayout(null);
         setResizable(false);
-        setTitle("Tarefas");
+        setTitle("Lista Tarefas");
         setSize(345,330);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         configurarJanela();
@@ -24,32 +22,30 @@ public class Janela extends JFrame {
 
     public void configurarJanela(){
         JPanel painel = new JPanel();
-        //painel.setBackground(Color.CYAN);
+        //painel.setBackground(Color.Blue);
         painel.setLayout(new FlowLayout(FlowLayout.CENTER,60,10));
         painel.setBounds(0,0,345,50);
-
         labelcaixa = new JLabel("Caixa de entrada");
-
         tarefaButton = new JButton("Nova Tarefa");
 
         JPanel painel2 = new JPanel();
-        //painel2.setBackground(Color.GREEN);
+        //painel2.setBackground(Color.Red);
         painel2.setBounds(0,50,345,280);
         painel2.setLayout(new BoxLayout(painel2,BoxLayout.PAGE_AXIS));
 
-        Tarefa = new JCheckBox("Comprar Carvão");
+        Tarefa = new JCheckBox("Comprar Picanha");
         Tarefa.setFocusable(false);
         Tarefa.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        Tarefa1 = new JCheckBox("Comprar Carvão");
+        Tarefa1 = new JCheckBox("Comprar Picanha");
         Tarefa1.setFocusable(false);
         Tarefa1.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        Tarefa2 = new JCheckBox("Comprar Carvão");
+        Tarefa2 = new JCheckBox("Comprar Picanha");
         Tarefa2.setFocusable(false);
         Tarefa2.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        Tarefa3 = new JCheckBox("Comprar Carvão");
+        Tarefa3 = new JCheckBox("Comprar Picanha");
         Tarefa3.setFocusable(false);
         Tarefa3.setAlignmentX(Component.CENTER_ALIGNMENT);
 
@@ -64,8 +60,6 @@ public class Janela extends JFrame {
         painel2.add(Tarefa2);
         painel2.add(Box.createRigidArea(new Dimension(10,10)));
         painel2.add(Tarefa3);
-
-
         add(painel);
         add(painel2);
     }
